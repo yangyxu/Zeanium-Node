986d92c6c5133b70fffce50d5e7ff914dc572554
yangyxu <945758912@qq.com>
2017-09-05 09:43:55 +0800

    fix common Model issue

--- src/db/common/model/Tag.js
+++ src/db/common/model/Tag.js
@@ -2,7 +2,13 @@ zn.define(function () {
 
     return zn.Class("zn.db.common.model.Tag", zn.db.data.Model, {
         properties: {
-            zn_tag_vars: {
+            zn_tags: {
+                value: null,
+                type: ['varchar', 500],
+                ignore: true,
+                default: ','
+            },
+            zn_tags_ids: {
                 value: null,
                 type: ['varchar', 250],
                 ignore: true,
--- src/db/common/model/Tree.js
+++ src/db/common/model/Tree.js
@@ -6,19 +6,19 @@ zn.define('../collection/Tree',function (Tree) {
             zn_tree_pid: {
                 value: null,
                 type: ['int', 11],
-                default: '0'
+                default: 0
             },
             zn_tree_depth: {
                 value: null,
                 type: ['int', 11],
                 ignore: true,
-                default: '0'
+                default: 0
             },
             zn_tree_order: {
                 value: null,
                 type: ['int', 11],
                 ignore: true,
-                default: '0'
+                default: 0
             },
             zn_tree_parent_path: {
                 value: null,
@@ -30,13 +30,13 @@ zn.define('../collection/Tree',function (Tree) {
                 value: null,
                 type: ['int', 11],
                 ignore: true,
-                default: '0'
+                default: 0
             },
             zn_tree_max_son_count: {
                 value: null,
                 type: ['int', 11],
                 ignore: true,
-                default: '0'
+                default: 0
             },
             zn_tree_extend: {
                 value: null,
@@ -46,7 +46,7 @@ zn.define('../collection/Tree',function (Tree) {
             zn_tree_type: {
                 value: null,
                 type: ['int', 11],
-                default: '0'
+                default: 0
             }
         }
     });
